b29ec56824b56ee53b9b0bd40a1543af41fb8926
Kurt Dirnbauer <dirnbauer@webconsulting.at>
2026-03-07 05:47:08 +0100

    [TASK] Move Extbase persistence mapping from TypoScript to PHP
    Replaces the deprecated plugin.tx_pwteaser.persistence.classes
    TypoScript configuration with the modern
    Configuration/Extbase/Persistence/Classes.php approach.
    
    Removes ext_typoscript_setup.typoscript and empties the TypoScript
    setup import since persistence was its only content.

--- Configuration/Extbase/Persistence/Classes.php
+++ Configuration/Extbase/Persistence/Classes.php
@@ -1,35 +1,64 @@
-<?php declare(strict_types=1);
+<?php
 
-use PwTeaserTeam\PwTeaser\Domain\Model\Content;
+declare(strict_types=1);
+
 use PwTeaserTeam\PwTeaser\Domain\Model\Page;
-
-// This configuration is used for TYPO3 10 and later
-// Previous versions use "ext_typoscript_setup.txt" file in root of extension.
+use PwTeaserTeam\PwTeaser\Domain\Model\Content;
 
 return [
     Page::class => [
         'tableName' => 'pages',
         'properties' => [
-            'navTitle' => ['fieldName' => 'nav_title'],
-            'authorEmail' => ['fieldName' => 'author_email'],
-            'tstamp' => ['fieldName' => 'tstamp'],
-            'creationDate' => ['fieldName' => 'crdate'],
-            'lastUpdated' => ['fieldName' => 'lastUpdated'],
-            'starttime' => ['fieldName' => 'starttime'],
-            'endtime' => ['fieldName' => 'endtime'],
-            'newUntil' => ['fieldName' => 'newUntil'],
-            'sorting' => ['fieldName' => 'sorting'],
-            'l18nConfiguration' => ['fieldName' => 'l18n_cfg'],
-        ]
+            'navTitle' => [
+                'fieldName' => 'nav_title',
+            ],
+            'authorEmail' => [
+                'fieldName' => 'author_email',
+            ],
+            'tstamp' => [
+                'fieldName' => 'tstamp',
+            ],
+            'creationDate' => [
+                'fieldName' => 'crdate',
+            ],
+            'lastUpdated' => [
+                'fieldName' => 'lastUpdated',
+            ],
+            'starttime' => [
+                'fieldName' => 'starttime',
+            ],
+            'endtime' => [
+                'fieldName' => 'endtime',
+            ],
+            'newUntil' => [
+                'fieldName' => 'newUntil',
+            ],
+            'sorting' => [
+                'fieldName' => 'sorting',
+            ],
+            'l18nConfiguration' => [
+                'fieldName' => 'l18n_cfg',
+            ],
+        ],
     ],
     Content::class => [
         'tableName' => 'tt_content',
         'properties' => [
-            'pid' => ['fieldName' => 'pid'],
-            'colPos' => ['fieldName' => 'colPos'],
-            'ctype' => ['fieldName' => 'CType'],
-            'tstamp' => ['fieldName' => 'tstamp'],
-            'crdate' => ['fieldName' => 'crdate'],
-        ]
-    ]
+            'pid' => [
+                'fieldName' => 'pid',
+            ],
+            'colPos' => [
+                'fieldName' => 'colPos',
+            ],
+            'ctype' => [
+                'fieldName' => 'CType',
+            ],
+            'tstamp' => [
+                'fieldName' => 'tstamp',
+            ],
+            'crdate' => [
+                'fieldName' => 'crdate',
+            ],
+        ],
+    ],
 ];
