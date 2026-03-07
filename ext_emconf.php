1038cfc31aa17e194f6547a04ce5b8c9d792b4b8
Kurt Dirnbauer <dirnbauer@webconsulting.at>
2026-03-07 09:55:18 +0100

    [TASK] Update ext_emconf.php version constraints
    Align ext_emconf.php TYPO3 version constraints with the updated
    composer.json requirements, supporting both TYPO3 13 LTS and
    TYPO3 14.

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Page Teaser (with Fluid)',
     'description' => 'Create powerful page teasers in TYPO3 CMS with data from page properties and its content elements. Based on Extbase and Fluid template engine.',
     'category' => 'plugin',
-    'version' => '6.0.3',
+    'version' => '7.0.0',
     'state' => 'stable',
     'author' => 'Armin Vieweg',
     'author_email' => 'info@v.ieweg.de',
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '8.2.0-8.4.99',
-            'typo3' => '13.4.0-13.4.99',
+            'typo3' => '13.4.0-14.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
